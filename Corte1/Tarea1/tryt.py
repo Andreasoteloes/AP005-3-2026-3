10a1d5d5d520825098c97fa78d033deee124f0c2
Andreasoteloes <143759043+Andreasoteloes@users.noreply.github.com>
2026-09-20 15:52:13 -0500

    Update tryt.py

--- Corte1/Tarea1/tryt.py
+++ Corte1/Tarea1/tryt.py
@@ -1,2 +1,9 @@
+# 1. Realizamos una operación matemática combinada con una comparación lógica:
+# Primero se suma 7 + 8 (que da como resultado 15).
+# Luego, se evalúa si ese 15 es estrictamente igual (==) a 11.
+# Como 15 no es igual a 11, la pregunta arroja un valor de verdad Falso (False).
 my_cool_variable = 7 + 8 == 11
+
+# 2. Imprimimos en la consola el valor almacenado en la variable, 
+# el cual será False.
 print(my_cool_variable)
